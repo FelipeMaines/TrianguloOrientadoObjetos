8d0f9cf8cc4bba91001d053b29799ae4ecbae080
Felipe Maines <felipemaines123@gmail.com>
2023-04-05 17:16:50 -0300

    Add project files.

---
+++ TrianguloOrientadoObjetos.ConsoleAPP/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace TrianguloOrientadoObjetos.ConsoleAPP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Trinagulo trinagulo = new Trinagulo();
+
+            Console.Write("Qual o lado 1: ");
+            trinagulo.lado1 = int.Parse(Console.ReadLine());
+
+            Console.Write("\nQual o lado 2: ");
+            trinagulo.lado2 = int.Parse(Console.ReadLine());
+
+            Console.Write("\nQual o lado 3: ");
+            trinagulo.lado3 = int.Parse(Console.ReadLine());
+
+            trinagulo.VerificarSePodeSerTrinagulo();
+        }
+    }
+}
---
+++ TrianguloOrientadoObjetos.ConsoleAPP/Trinagulo.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrianguloOrientadoObjetos.ConsoleAPP
+{
+    public class Trinagulo
+    {
+
+        public int lado1;
+        public int lado2;
+        public int lado3;
+        public bool podeSerTringulo = false;
+        
+       public void VerificarSePodeSerTrinagulo()
+        {
+            if (lado1 + lado2 < lado3 || lado2 + lado3 < lado1 || lado3 + lado1 < lado2)
+            {
+                Console.WriteLine("Nao pode ser um triangulo"); 
+                
+            }
+
+            else
+            {
+                podeSerTringulo = true;
+                VerificarTipoTriangulo();
+                
+            }
+        }
+
+        public void VerificarTipoTriangulo()
+        {
+            if (podeSerTringulo)
+            {
+                if(lado1 != lado2 && lado2 != lado3)
+                {
+                    Console.WriteLine("Eh um Trinagulo Escaleno"); 
+                }
+
+                else if (lado1 == lado2 && lado2 == lado3)
+                {
+                    Console.WriteLine("Eh um tringulo Equilatero");
+                }
+
+                else if (lado1 == lado2 && lado2 != lado3 || lado2 == lado3 && lado2 != lado1 || lado1 == lado3 && lado3 != lado2)
+                {
+                    Console.WriteLine("Eh um triangulo Isoceles");
+                }
+            }
+        }
+    }
+}
